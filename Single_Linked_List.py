570a587af41c675afbebe2e4f0e24054e3b59b86
Sunny <sunnyyadav13091999@gmail.com>
2021-07-30 10:33:29 +0530

    First

--- Single_Linked_List.py
+++ Single_Linked_List.py
@@ -15,7 +15,24 @@ class LinkedList:
             while n is not None:
                 print(n.data)
                 n = n.ref
+
     def add_begin(self,data):
         new_node = Node(data)
         new_node.ref = self.head
         self.head = new_node
+
+    def add_end(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.ref is not None:
+                n = n.ref
+            n.ref = new_node
+
+LL1=LinkedList()
+LL1.add_begin(10)
+LL1.add_end(100)
+LL1.add_begin(20)
+LL1.print_LL()
