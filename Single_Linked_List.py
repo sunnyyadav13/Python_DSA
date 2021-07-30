e019de03894301198f52546c16a26ca2986ae935
Sunny <sunnyyadav13091999@gmail.com>
2021-07-30 10:25:56 +0530

    first

---
+++ Single_Linked_List.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.ref = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_LL(self):
+        if self.head is None:
+            print("Linked list is empty!")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data)
+                n = n.ref
+    def add_begin(self,data):
+        new_node = Node(data)
+        new_node.ref = self.head
+        self.head = new_node
